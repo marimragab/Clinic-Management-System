07461f36dec81cc5d3c5384a8d0ba704d9e79cd4
Eman Magdi <103532554+EmanMagdi-dev@users.noreply.github.com>
2023-02-07 22:01:25 +0200

    Update employee.js

--- Controllers/employee.js
+++ Controllers/employee.js
@@ -13,7 +13,7 @@ exports.getAllEmployees=(request,response,next)=>{
     .catch(error=>next(error))
 } 
 
-// add new teacher with status 201
+
 exports.addEmployee=(request,response,next)=>{
     let newEmployee=new employeeSchema({
         fullName:request.body.fullName,
@@ -30,10 +30,10 @@ exports.addEmployee=(request,response,next)=>{
 }
   
 
-// update teacher 
+
 exports.updateEmployee=(request,response,next)=>{
         employeeSchema.updateOne({
-            _id:request.body._id    //get id
+            _id:request.body._id    
 },{
     $set:{fullName:request.body.fullName,
             address:request.body.address,
@@ -41,28 +41,28 @@ exports.updateEmployee=(request,response,next)=>{
             age:request.body.age,
             roll:request.body.roll}, 
 }).then(result=>{
-    response.status(200).json(result)       //succedd
+    response.status(200).json(result)       
 })
-.catch(error=>next(error))          //error
+.catch(error=>next(error))         
 }
 
 // delete teacher 
 exports.deleteEmployee=(request,response,next)=>{
     employeeSchema.deleteOne({
-        _id:request.body._id    //get id
+        _id:request.body._id    
 })
 .then(result=>{
-    response.status(200).json(result)       //succedd
+    response.status(200).json(result)       
 })
-.catch(error=>next(error))          //error
+.catch(error=>next(error))          
 }
 
-//get one teacher by Id
+
 exports.getEmployeeByID=(request,response,next)=>{
 
     employeeSchema.findOne({_id:request.params.id})
     .then(data=>{
-        if(data != null)             //mean found data so return data
+        if(data != null)             
             response.status(200).json(data)
            
         else
