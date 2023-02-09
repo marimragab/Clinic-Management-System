4c622091e5bca1b3543ef03e72a6dc2e151b80af
marimragab <maryamragab@outlook.sa>
2023-02-09 11:45:29 +0200

    update invoice validations

--- Validations/invoice.js
+++ Validations/invoice.js
@@ -1,23 +1,36 @@
 const { param, body, query } = require("express-validator");
+const mongoose = require("mongoose");
+require("./../Models/patient");
+const Patient = mongoose.model("patients");
+require("./../Models/employee");
+const Employee = mongoose.model("employees");
+require("./../Models/clinicServices");
+const ClinicServices = mongoose.model("clinicServices");
+const Medicine = require("../Models/medicine");
 
 const addInvoiceValidations = [
   body("patient")
     .notEmpty()
     .withMessage("Patient id is required")
-    .isMongoId()
-    .withMessage("Patient id must be objectId"),
+    .isInt()
+    .withMessage("Patient id must be number")
+    .custom(async (value) => {
+      const patient = await Patient.findOne({ _id: value });
+      if (!patient) {
+        throw new Error("There is no patient with entered id");
+      }
+    }),
   body("employee")
     .notEmpty()
     .withMessage("Employee id is required")
     .isInt()
-    .withMessage("Employee id must be number"),
-  body("date")
-    .notEmpty()
-    .withMessage("Invoice Date is required")
-    .isDate()
-    .withMessage(
-      "Invoice Date must be a valid date on format YYYY/MM/DD or YYYY-MM-DD"
-    ),
+    .withMessage("Employee id must be number")
+    .custom(async (value) => {
+      const employee = await Employee.findOne({ _id: value });
+      if (!employee) {
+        throw new Error("There is no employee with entered id");
+      }
+    }),
   body("servedServices")
     .notEmpty()
     .withMessage("Invoice Services is required")
@@ -25,35 +38,93 @@ const addInvoiceValidations = [
     .withMessage("Invoice Service must be array")
     .isLength({ min: 1 })
     .withMessage("Served Services must contain at least one service data"),
-  body("servedServices.*.info")
+  body("servedServices.*.serviceinfo")
     .notEmpty()
     .withMessage("Invoice Serves is required")
     .isInt()
-    .withMessage("Service info must be number"),
-  body("servedServices.*.amount")
+    .withMessage("Service info must be number")
+    .custom(async (value) => {
+      const service = await ClinicServices.findOne({ _id: value });
+      if (!service) {
+        throw new Error("There is no service with entered id");
+      }
+    }),
+  body("servedServices.*.count")
     .notEmpty()
-    .withMessage("Invoice amount is required")
-    .isInt()
+    .withMessage("Service count is required")
+    .isInt({ min: 1 })
     .withMessage("Service amount must be number"),
+  body("medicines")
+    .notEmpty()
+    .withMessage("Invoice Services is required")
+    .isArray()
+    .withMessage("Invoice Service must be array")
+    .isLength({ min: 1 })
+    .withMessage("Served Services must contain at least one service data")
+    .optional(),
+  body("medicines.*.medicine")
+    .notEmpty()
+    .withMessage("Medicine id is required")
+    .isInt({ min: 1 })
+    .withMessage("Medicine id must be number")
+    .custom(async (value) => {
+      const medicine = await Medicine.findOne({ _id: value });
+      if (!medicine) {
+        throw new Error("There is no medicine with entered id");
+      }
+    })
+    .optional(),
+  body("medicines.*.count")
+    .notEmpty()
+    .withMessage("Medicine number is required")
+    .isInt()
+    .withMessage("Medicine number must be number")
+    .optional(),
+  //! will calculated from calculating services and medicines
+  // body("total")
+  //   .notEmpty()
+  //   .withMessage("Total invoice charge is required")
+  //   .isInt()
+  //   .withMessage("Invoice total charge must be number"),
+  body("paymentMethod")
+    .notEmpty()
+    .withMessage("Payment Method is required")
+    .isIn(["Cash", "Credit Card", "Insurance Card"])
+    .withMessage(
+      "Appointment type must be one of Cash, Credit Card,Insurance Card"
+    ),
 ];
 
 const updateInvoiceValidations = [
   body("id")
     .notEmpty()
     .withMessage("Invoice id is required")
-    .isMongoId()
-    .withMessage("Invoice id must be objectId"),
+    .isInt()
+    .withMessage("Invoice id must be number"),
+  //!Are we need to update patient or employee data??I think the only case if there is syntax error on data not the patient or employee itself changes
   body("patient")
     .notEmpty()
     .withMessage("Patient id is required")
-    .isMongoId()
-    .withMessage("Patient id must be objectId")
+    .isInt()
+    .withMessage("Patient id must be number")
+    .custom(async (value) => {
+      const patient = await Patient.findOne({ _id: value });
+      if (!patient) {
+        throw new Error("There is no patient with entered id");
+      }
+    })
     .optional(),
   body("employee")
     .notEmpty()
     .withMessage("Employee id is required")
     .isInt()
     .withMessage("Employee id must be number")
+    .custom(async (value) => {
+      const employee = await Employee.findOne({ _id: value });
+      if (!employee) {
+        throw new Error("There is no employee with entered id");
+      }
+    })
     .optional(),
   body("date")
     .notEmpty()
@@ -70,15 +141,21 @@ const updateInvoiceValidations = [
     .withMessage("Invoice Service must be array")
     .isLength({ min: 1 })
     .withMessage("Served Services must contain at least one service data"),
-  body("servedServices.*.info")
+  body("servedServices.*.serviceinfo")
     .notEmpty()
     .withMessage("Invoice Serves is required")
     .isInt()
     .withMessage("Service info must be number")
+    .custom(async (value) => {
+      const service = await ClinicServices.findOne({ _id: value });
+      if (!service) {
+        throw new Error("There is no service with entered id");
+      }
+    })
     .optional(),
   body("servedServices.*.count")
     .notEmpty()
-    .withMessage("Invoice amount is required")
+    .withMessage("Service count is required")
     .isInt()
     .withMessage("Service amount must be number")
     .optional(),
@@ -88,25 +165,45 @@ const updateInvoiceValidations = [
     .isArray()
     .withMessage("Invoice Service must be array")
     .isLength({ min: 1 })
-    .withMessage("Served Services must contain at least one service data"),
-  body("servedServices.*.info")
-    .notEmpty()
-    .withMessage("Invoice Serves is required")
-    .isInt()
-    .withMessage("Service info must be number")
+    .withMessage("Served Services must contain at least one service data")
     .optional(),
+  body("medicines.*.medicine")
+    .notEmpty()
+    .withMessage("Medicine id is required")
+    .isInt()
+    .withMessage("Medicine id must be number")
+    .custom(async (value) => {
+      const medicine = await Medicine.findOne({ _id: value });
+      if (!medicine) {
+        throw new Error("There is no medicine with entered id");
+      }
+    })
+    .optional(),
+  body("medicines.*.count")
+    .notEmpty()
+    .withMessage("Medicine number is required")
+    .isInt()
+    .withMessage("Medicine number must be number")
+    .optional(),
+  body("paymentMethod")
+    .notEmpty()
+    .withMessage("Payment Method is required")
+    .isIn(["Cash", "Credit Card", "Insurance Card"])
+    .withMessage(
+      "Appointment type must be one of Cash, Credit Card,Insurance Card"
+    ),
 ];
 
-const deleteInvoiceValidations = [
+const idParamInvoiceValidations = [
   param("id")
     .notEmpty()
     .withMessage("Invoice id is required")
-    .isMongoId()
-    .withMessage("Invoice id must be objectId"),
+    .isInt()
+    .withMessage("Invoice id must be number"),
 ];
 
 module.exports = {
   addInvoiceValidations,
   updateInvoiceValidations,
-  deleteInvoiceValidations,
+  idParamInvoiceValidations,
 };
